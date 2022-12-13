1f7fb94040b00b9c77cc7f3d46869617af10e640
Samir Omer <samiromer2@yahoo.com>
2022-12-13 20:22:58 +0200

    betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
- * Description: a function that prints the alphabet,
- * in lowecase followed by a new line
- * Return: void
+ * main - Entry point
+ * 
+ * Return: 0 nothing,return void
  */
+
 int main(void)
 
 {
