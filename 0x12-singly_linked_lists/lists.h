78c2a1e60e7e9dfb30e0fc14086569ae290aa15d
Samir Omer <samiromer2@yahoo.com>
2023-01-26 22:06:59 +0200

    a check up

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/*
+* Desc: header file containinf prototypes and definitions
+*	for all functions and types written in this proj
+*/
+
 #include <stdlib.h>
 
 /**
@@ -10,7 +15,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *              for project
+ *              for Holberton project
  */
 typedef struct list_s
 {
